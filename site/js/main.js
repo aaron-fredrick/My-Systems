c3018959e1ccbc970858688e43ea038a13b84b0d
Aaron Fredrick <Aaron_Fredrick@proton.me>
2026-08-30 23:14:58 +0530

    feat: wire interactive topology into motion runtime

--- site/js/main.js
+++ site/js/main.js
@@ -2,8 +2,10 @@ import { initScrollMotion } from './scroll.js';
 import { initPointerMotion } from './pointer.js';
 import { initSystems } from './systems.js';
 import { initGlobe } from './globe.js';
+import { initTopology } from './topology.js';
 
 initScrollMotion();
 initPointerMotion();
 initSystems();
+initTopology();
 initGlobe();
